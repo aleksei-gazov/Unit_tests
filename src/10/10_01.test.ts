5a9b2c5606bf8f6af0815aaf32e0decab6af2c5e
Aleksei <98910189+09121988stroitel1@users.noreply.github.com>
2023-01-19 15:33:59 +0300

    Update 10_01.test.ts

---
+++ src/10/10_01.test.ts
@@ -0,0 +1,28 @@
+import { createGretingMessage, ManType } from "./05";
+
+
+let people: Array<ManType>= [
+  {name: "Andrey Ivanov", age: 7},
+  {name: "Vavan Rog", age: 74},
+  {name: "Lysia Pipa", age: 67}
+]
+
+beforeEach( ()=> {
+  people = [
+    {name: "Andrey Ivanov", age: 7},
+    {name: "Vavan Rog", age: 74},
+    {name: "Lysia Pipa", age: 67}
+  ]
+})
+
+test('should be array of greting message', ()=> {
+
+  const messages = createGretingMessage(people)
+
+expect(messages.length).toBe(3)
+expect(messages[0]).toBe('Hello Andrey. Welcom to Mogilev')
+expect(messages[1]).toBe('Hello Vavan. Welcom to Mogilev')
+expect(messages[2]).toBe('Hello Lysia. Welcom to Mogilev')
+
+
+})
